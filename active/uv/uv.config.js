6b0b667f707bf2a75debfa1714ee2dbd9ce4bf75
Aeos Network <aeosntw@gmail.com>
2025-09-27 13:53:10 -0400

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare:'https://qms.diddy.ptchat.net/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/active/uv/uv.handler.js',
-    bundle: '/active/uv/uv.bundle.js',
-    config: '/active/uv/uv.config.js',
-    sw: '/active/uv/uv.sw.js',
+    handler: '/t0mb/active/uv/uv.handler.js',
+    bundle: '/t0mb/active/uv/uv.bundle.js',
+    config: '/t0mb/active/uv/uv.config.js',
+    sw: '/t0mb/active/uv/uv.sw.js',
 };
