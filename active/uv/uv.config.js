d10ca94f48acc9428a6b4ae347e45ada5cd96234
Aeos Network <aeosntw@gmail.com>
2025-09-27 13:50:19 -0400

    fix

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,5 +1,5 @@
 self.__uv$config = {
-    prefix: '/active/go/',
+    prefix: '/t0mb/active/go/',
     bare:'https://qms.diddy.ptchat.net/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
